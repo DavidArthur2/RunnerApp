fe6fd2ee2cc38240d5c396cbfbf29a1e95fb436d
DavidArthur2 <david.arthur@student.ms.sapientia.ro>
2024-06-16 00:14:13 +0300

    Fix with the permissions Added ICON

--- app/src/main/java/com/festipay/runnerapp/activities/MainActivity.kt
+++ app/src/main/java/com/festipay/runnerapp/activities/MainActivity.kt
@@ -135,8 +135,6 @@ class MainActivity : AppCompatActivity() {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.INTERNET,
             Manifest.permission.CAMERA,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE
         )
 
         ActivityCompat.requestPermissions(activity, permissions, 101)
